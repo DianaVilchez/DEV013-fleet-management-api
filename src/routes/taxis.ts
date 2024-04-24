f25cf666d1918a386d69e4c0dd3049fa2e1c227d
Diana <educ.dana@gmail.com>
2024-04-24 09:25:07 -0500

    Creacion de la solicitud get,Implementacion de paginación del listado de taxis

--- src/routes/taxis.ts
+++ src/routes/taxis.ts
@@ -6,12 +6,14 @@ const router = Router()
 const prisma = new PrismaClient()
 
 router.get('/taxis',async(req,res) => {
- 
+ //implementacion de paginacion 
+ // se tiene que especificar el tipo en req.query._page y req.query._limit
  const page = parseInt(req.query._page as string, 10) || 1; // Página actual
  const limit = parseInt(req.query._limit as string, 10) || 10; // Tamaño de página
  const startIndex = (page - 1) * limit; // Índice de inicio
- 
+
  // Recuperar los usuarios de la página actual
+ //findMany: obtener todos los datos
  const taxis = await prisma.taxis.findMany()
 
  const currentPageUsers = taxis.slice(startIndex, startIndex + limit);
