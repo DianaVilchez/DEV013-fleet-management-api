7f684843630e26d7bcfe77b5bfb590d35cc1dcc5
Diana <educ.dana@gmail.com>
2024-05-16 02:21:29 -0500

    refactorización de codigo(taxis y trajectories) con paginacion offset

--- src/controllers/trajectories.ts
+++ src/controllers/trajectories.ts
@@ -1,131 +1,58 @@
 import { Request, Response } from "express";
 import { PrismaClient, /*trajectories*/} from "@prisma/client";
-import { handleHttp } from "../utils/error";
+// import { handleHttp } from "../utils/error";
 
 const prisma = new PrismaClient();
 // module.exports = {
 const getTrajectories = async (req: Request, res: Response) => {
   try {
     const { taxi_id, date } = req.query;
-
-    const lastId = parseInt(req.query.lastId as string, 10) || 0; // Página actual
+    const page = parseInt(req.query.page as string, 10) || 0; // Página actual
     const limit = parseInt(req.query.limit as string, 10) || 10; // Tamaño de página
-
+    const offset = (page - 1) * limit;
     console.log(taxi_id);
     console.log(date);
-
+    //obtener las 24 horas del dia
+    //getFullYear():obtiene el año,getMonth():el mes,getDate():el dia
+    //endofday tiene que ser un dia mas , ya que marca el limite de la fecha
     const dateList = new Date(date as string);
-    const starOfDay = new Date(
-      Date.UTC(
-        dateList.getUTCFullYear(),
-        dateList.getUTCMonth(),
-        dateList.getUTCDate()
-      )
-    );
-    const endOfDay = new Date(
-      Date.UTC(
-        dateList.getUTCFullYear(),
-        dateList.getUTCMonth(),
-        dateList.getUTCDate() + 1
-      )
-    );
-
-    let trajectoriesData;
-    if (taxi_id && date) {
-      trajectoriesData = await prisma.trajectories.findMany({
-        where: {
-          taxi_id: Number(taxi_id), // Convertir a número si es necesario
-          date: {
-            gte: starOfDay,
-            lt: endOfDay,
-          }, // Asegurarse de que la fecha sea válida
-        },
-        take: limit,
-        cursor: lastId ? { id: Number(lastId) } : undefined,
-        skip: lastId ? 1 : 0, // Omitir el registro actual del cursor para empezar después de este
-        orderBy: {
-          id: "asc",
-        },
-      });
-      console.log(trajectoriesData);
-      if (trajectoriesData.length > 0) {
-        return res.status(200).json({
-          data: trajectoriesData, // los registros solicitados
-          nextCursor:
-            trajectoriesData.length > 0
-              ? trajectoriesData[trajectoriesData.length - 1].id
-              : null, // el ID para la próxima solicitud de paginación
-          limit: limit,
-        });
-      }
-      return res.status(404).json({ message: "No se encontraron datos" });
-    }
+    const starOfDay = new Date(Date.UTC(dateList.getUTCFullYear(),dateList.getUTCMonth(),dateList.getUTCDate()));
+    const endOfDay = new Date(Date.UTC(dateList.getUTCFullYear(),dateList.getUTCMonth(),dateList.getUTCDate() + 1));
+    let whereOptions ={};
+    if(date === "" || taxi_id===""){
+      return res.status(400).json({message:"Dato no insertado"})
+  }
     if (date) {
       console.log("hola");
-      //obtener las 24 horas del dia
-      //getFullYear():obtiene el año,getMonth():el mes,getDate():el dia
-      //endofday tiene que ser un dia mas , ya que marca el limite de la fecha
       console.log(starOfDay);
-      trajectoriesData = await prisma.trajectories.findMany({
-        where: {
-          // taxi_id: Number(taxi_id), // Convertir a número si es necesario
-          date: {
-            gte: starOfDay,
-            lt: endOfDay,
-          }, // Asegurarse de que la fecha sea válida
-        },
-        take: limit,
-        cursor: lastId ? { id: Number(lastId) } : undefined,
-        skip: lastId ? 1 : 0, // Omitir el registro actual del cursor para empezar después de este
-        orderBy: {
-          id: "asc",
-        },
-      });
-      console.log("hola2");
-      console.log(trajectoriesData);
-      if (trajectoriesData.length > 0) {
-        return res.status(200).json({
-          data: trajectoriesData, // los registros solicitados
-          nextCursor:
-            trajectoriesData.length > 0
-              ? trajectoriesData[trajectoriesData.length - 1].id
-              : null, // el ID para la próxima solicitud de paginación
-          limit: limit,
-        });
+      whereOptions= {...whereOptions,
+        date: {
+          gte: starOfDay,
+          lt: endOfDay,
+        }, // Asegurarse de que la fecha sea válida
       }
-      return res.status(404).json({ message: "No se encontraron datos" });
     }
-    if (taxi_id) {
-      trajectoriesData = await prisma.trajectories.findMany({
-        where: {
-          taxi_id: Number(taxi_id), // Convertir a número si es necesario
-        },
-        take: limit,
-        cursor: lastId ? { id: Number(lastId) } : undefined,
-        skip: lastId ? 1 : 0, // Omitir el registro actual del cursor para empezar después de este
-        orderBy: {
-          id: "asc",
-        },
+    if (taxi_id) { 
+      whereOptions= { ...whereOptions, taxi_id: Number(taxi_id) }  
+    }
+    const trajectoriesData = await prisma.trajectories.findMany({
+      where: whereOptions,
+      skip: offset,
+      take: limit,
+      orderBy: { id: "asc" },
+    });
+    if (trajectoriesData.length > 0) {
+      return res.status(200).json({
+        data: trajectoriesData,
+        page: page,
+        limit: limit,
       });
-      console.log(trajectoriesData);
-      if (trajectoriesData.length > 0) {
-        return res.status(200).json({
-          data: trajectoriesData, // los registros solicitados
-          nextCursor:
-            trajectoriesData.length > 0
-              ? trajectoriesData[trajectoriesData.length - 1].id
-              : null, // el ID para la próxima solicitud de paginación
-          limit: limit,
-        });
-      }
+    } else {
       return res.status(404).json({ message: "No se encontraron datos" });
-    }
-
-    if (!taxi_id || !date) {
-      return res.status(400).json({ message: "Faltan datos" });
     }
   } catch (error) {
-    return handleHttp(res, "Error en el servidor");
+    return res.status(500).json({message:"Error en el servidor"}) ;
+    // return handleHttp(res, "Error en el servidor");
   }
 };
 
---
+++ test/e2e.test.ts
@@ -0,0 +1,84 @@
+import request from "supertest";
+// import { getTrajectories } from "../src/controllers/trajectories";
+const app = require('../src/app');//para que se pueda usar supertest se llama al archivo principal
+
+// describe('GET /trajectories', () => {
+//   it('It should return all the trajectories of the corresponding taxi_id.',() => (
+//     fetch('/trajectories').then((res) => expect(res.status).toBe(200))
+//   ))
+// })
+
+describe("/GET/taxis", () => {
+  it("Deberia devolver el listado de taxis paginado con las propiedades page y limit", async () => {
+    const res = await request(app).get("/taxis")
+    .query({ page: "1", limit: "10" });
+    expect(res.status).toBe(200);
+    expect(res.body).toHaveProperty("page");
+    expect(res.body).toHaveProperty("limit");
+  });
+  it("Deberia devolver el taxi obtenido con el taxi_id", async () => {
+    const res = await request(app).get("/taxis").query({ taxi_id: 10133 });
+    expect(res.status).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.id).toBe(10133);
+  });
+  it("Deberia devolver el taxi obtenido con el plate", async () => {
+    const res = await request(app).get("/taxis").query({ plate: "FNHK-3772" });
+    expect(res.status).toBe(200);
+    expect(res.body).toBeDefined();
+    expect(res.body.plate).toEqual("FNHK-3772"); // Asegúrate de que la respuesta incluya la placa buscada
+  });
+  it("Debería devolver error 400 si 'plate' está vacío", async () => {
+    const res = await request(app)
+      .get("/taxis")
+      .query({ plate: "", taxi_id: "" });
+    console.log("Status Code:", res.status);
+    console.log("Body:", res.body); // Esto te mostrará qué contiene realmente el cuerpo
+    console.log("Text:", res.text); // Esto puede ayudar si body está vacío
+    expect(res.status).toBe(400);
+    expect(res.body).toEqual({ message: "Dato no insertado" });
+  });
+  it("Debería devolver error 404 si los datos son erroneos", async () => {
+    const res = await request(app).get("/taxis").query({ plate: "FNHK-377" });
+    expect(res.status).toBe(404);
+    expect(res.body).toEqual({ message: "Taxi no encontrado" });
+  });
+  it("Deberia devolver error 500",async() => {
+    const res = await request(app).get("/taxis");
+    expect(res.status).toBe(500); 
+    expect(res.body).toEqual({message:"Error en el servidor"});
+  })
+
+  // it('debería manejar un error interno del servidor',async() => {
+  //       const res =await(app)
+  //       .get("/taxis");
+  //       // expect(res.status).toBe(500);
+  //       expect(res.body).toHaveProperty('message','Error en el servidor');
+  // });
+});
+
+describe("GET /trajectories",() =>{
+  it("It should return all the trajectories of the corresponding taxi_id" ,async()=> {
+    const res = await request(app)
+       .get("/trajectories")
+       .query({taxi_id:10133});
+       expect(res.status).toBe(200);
+       expect(res.body).toHaveProperty('data');
+  });
+
+  it("It should return all the trajectories of the corresponding taxi_id" ,async()=> {
+    const res = await request(app)
+       .get("/trajectories")
+       .query({date:"2008-02-04"});
+       expect(res.status).toBe(200);
+       expect(res.body).toHaveProperty('data');
+  });
+
+  it("It should return all the trajectories of the corresponding taxi_id" ,async()=> {
+    const res = await request(app)
+       .get("/trajectories")
+       .query({taxi_id:10133,date:"2008-02-04"});
+       expect(res.status).toBe(200);
+       expect(res.body).toHaveProperty('data');
+  });
+});
